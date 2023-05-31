0c41fe4b49be063f1e5968e5a3e18b92eb767ca4
josePereiro <jose.pereiro.personal@gmail.com>
2023-05-31 00:55:12 -0400

    New version

---
+++ src/NutrientLimitedGEMs.jl
@@ -0,0 +1,16 @@
+module NutrientLimitedGEMs
+
+    using ProjFlows
+    using Gurobi
+    using MetX
+    using Plots
+
+    #! include .
+    
+    #! include Base
+    # include("Base/ep_approx.jl")
+    # include("Base/force_nut_lim.jl")
+    # include("Base/plotutils.jl")
+    # include("Base/utils.jl")
+
+end
