cbddc05ee9dc7f1420323d3e6b2c5af678461ecb
josePereiro <jose.pereiro.personal@gmail.com>
2023-05-31 00:54:18 -0400

    New version

--- scripts/IJR904/0_params.jl
+++
@@ -1,7 +0,0 @@
-# TODO: reproduce the analysis done using ecoli_core (see DEPRECATED)
-## ------------------------------------------------------------------
-LP_SOLVER = Gurobi.Optimizer
-KO_FACTOR = 1e-2
-
-## ------------------------------------------------------------------
-SIM_ID = "EPv9"
