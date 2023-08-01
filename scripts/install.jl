1ac8b824a50f4141f022147def32cdb4b53b2b25
josePereiro <jose.pereiro.personal@gmail.com>
2023-08-01 01:48:08 -0400

    add install

---
+++ scripts/install.jl
@@ -0,0 +1,10 @@
+import Pkg
+
+## ---------------------------------------------------
+proj = Pkg.project()
+proj.name == "NutrientLimitedGEMs" && error("Bad project, ", proj.name)
+
+## ---------------------------------------------------
+todev = ["ContextDBs", "ImgTools", "MetX", "MetXBase", "MetXCultureHub", "MetXEP", "MetXNetHub", "MetXOptim", "ProjFlows"]
+Pkg.develop(todev)
+Pkg.precompile()
