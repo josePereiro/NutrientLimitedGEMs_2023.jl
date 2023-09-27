30befce96dd6f24e112e51c2b8b2c728b150f69f
josePereiro <jose.pereiro.personal@gmail.com>
2023-09-27 19:10:02 -0400

    WIP: GEM scripts

--- .cluster/runner.sh
+++ .cluster/runner.sh
@@ -1,8 +1,8 @@
 (julia -t5 --project scripts/BEG2007/PHASES_KOMA/7_core_fva.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_I-0.1.0") &
 (julia -t5 --project scripts/BEG2007/PHASES_KOMA/7_shadow_price.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_II-0.1.0") &
 (julia -t5 --project scripts/BEG2007/PHASES_KOMA/2.1_core_xlep0.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_I-0.1.0") &
+(julia -t5 --project scripts/BEG2007/PHASES_KOMA/3.3_rxn_map.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_I-0.1.0") &
+(julia -t5 --project scripts/BEG2007/PHASES_KOMA/3.2_gem_xlep0.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_I-0.1.0") &
 (julia -t5 --project scripts/BEG2007/PHASES_KOMA/9_biomass_fba.jl -- "SIMVER:ECOLI-CORE-BEG2007-PHASE_III-0.1.0") &
 julia -t5 --project scripts/BEG2007/PHASES_KOMA/_summary.jl  --  "SIMVER:"
 (julia -t5 --project scripts/BEG2007/PHASES_KOMA/6.2_plots.jl  --  "SIMVER:") &
-
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
