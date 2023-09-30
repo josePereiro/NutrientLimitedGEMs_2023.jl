a748d7af57048697c554352cd9fb79e1fa4d89fb
josePereiro <jose.pereiro.personal@gmail.com>
2023-09-30 12:40:04 -0400

    WIP

--- scripts/BEG2007/PHASES_KOMA/13_expression_corr.jl
+++ scripts/BEG2007/PHASES_KOMA/13_expression_corr.jl
@@ -20,16 +20,15 @@ include("1_setup.jl")
 include("2_utils.jl")
 
 ## --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
-function _microarray_patts()
+function _microarray_patts(;phs = 1:3)
     # load micorarray data
     array_db = query(["ROOT", "PHASES_MICROARRAY"])
     RAW_EXP_MAT = array_db["exp_mat"]
     MICRO_PATTS = Dict()
     for rxnid in keys(RAW_EXP_MAT)
         _patt = sum(RAW_EXP_MAT[rxnid]; dims = 1)
-        _patt .= _patt ./ mean(_patt)
         _patt = [mean(_patt[1:4]), mean(_patt[5:8]), mean(_patt[9:12])]
-        MICRO_PATTS[rxnid] = _patt
+        MICRO_PATTS[rxnid] = _patt[phs]
     end
     return MICRO_PATTS
 end
@@ -68,12 +67,38 @@ function _ensems_patt(hits...)
     end
     ENS_PATTS
 end
-_ensems_patt(r"PHASE_1.*Uniform.*5000")
 # ECOLI-CORE-BEG2007-PHASE_1...Uniform...5000...<<h=10001632393434744400>>.jls
-## --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 # create ph expression and ense flux mats
 let
-    
+    _vs = Float64[]
+    _Es = Float64[]
+    _corrs = Float64[]
+    for it in 1:10
+        _ensems = _ensems_patt(r"PHASE_1.*Uniform.*5000", r"PHASE_3.*Uniform.*5000")
+        _microarray = _microarray_patts(;phs = [1,3])
+        _comm = intersect(Set(keys(_ensems)), Set(keys(_microarray)))
+        __vs = Float64[]
+        __Es = Float64[]
+        for rxn in _comm
+            for (v, E) in zip(_ensems[rxn], _microarray[rxn])
+                push!(__vs, v)
+                push!(__Es, E)
+            end
+        end
+        push!(_corrs, cor(__vs, __Es))
+        push!(_vs, __vs...)
+        push!(_Es, __Es...)
+    end
 
-    
+    f = Figure()
+    ax = Axis(f[1,1];)
+    _vs = abs.(_vs) .- mean(abs.(_vs))
+    _vs ./= std(_vs)
+    _Es = _Es .- mean(_Es)
+    _Es ./= std(_Es)
+    scatter!(ax, _vs, _Es; color = :black)
+    lines!(ax, _vs, _vs; color = :black)
+    @show cor(_vs, _Es)
+    f
 end
